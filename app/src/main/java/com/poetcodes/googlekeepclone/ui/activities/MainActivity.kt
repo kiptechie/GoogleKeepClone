39d2f47c3a540313c8d010633347ea68cc0fa6a2
Timothy Kipkorir Serem <kipkorirserem3@gmail.com>
2021-08-01 23:57:17 +0300

    [in-progress] some UI improvements

--- app/src/main/java/com/poetcodes/googlekeepclone/ui/activities/MainActivity.kt
+++ app/src/main/java/com/poetcodes/googlekeepclone/ui/activities/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.poetcodes.googlekeepclone.ui.activities
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.poetcodes.googlekeepclone.databinding.ActivityMainBinding
+import com.poetcodes.googlekeepclone.repository.DataState
 import com.poetcodes.googlekeepclone.ui.MainStateEvent
 import com.poetcodes.googlekeepclone.ui.view_models.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+
+
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -20,13 +28,48 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(com.poetcodes.googlekeepclone.R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val topLevelDestinations: MutableSet<Int> = HashSet()
+        topLevelDestinations.add(com.poetcodes.googlekeepclone.R.id.notesFragment)
+        topLevelDestinations.add(com.poetcodes.googlekeepclone.R.id.remindersFragment)
+        topLevelDestinations.add(com.poetcodes.googlekeepclone.R.id.editAddLabelsFragment)
+        topLevelDestinations.add(com.poetcodes.googlekeepclone.R.id.archiveFragment)
+        topLevelDestinations.add(com.poetcodes.googlekeepclone.R.id.deletedFragment)
+        val appBarConfiguration = AppBarConfiguration(topLevelDestinations, binding.drawerLayout)
+        binding.materialToolbar.setupWithNavController(navController, appBarConfiguration)
+        binding.navView.setupWithNavController(navController)
+
         subscribeObservers()
         mainViewModel.setStateEvent(MainStateEvent.NoteEvents)
         mainViewModel.setStateEvent(MainStateEvent.LabelEvents)
     }
 
     private fun subscribeObservers() {
-        // TODO("Not yet implemented")
+        mainViewModel.labelDataState.observe(this, { labelDataState ->
+            when (labelDataState) {
+                is DataState.Error -> {
+                    showToast("Error loading Labels!")
+                }
+                is DataState.Loading -> {
+                    // do nothing
+                }
+                is DataState.Success -> {
+                    val labels = labelDataState.data
+                    if (labels.isNotEmpty()) {
+                        //
+                    }
+                }
+            }
+        })
+    }
+
+    private fun showToast(message: String?) {
+        if (message == null) {
+            return
+        }
+        Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
     }
 
 }
--- app/src/main/java/com/poetcodes/googlekeepclone/ui/fragments/NotesFragment.kt
+++ app/src/main/java/com/poetcodes/googlekeepclone/ui/fragments/NotesFragment.kt
@@ -5,12 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.poetcodes.googlekeepclone.R
+import androidx.fragment.app.activityViewModels
+import com.poetcodes.googlekeepclone.databinding.FragmentNotesBinding
+import com.poetcodes.googlekeepclone.repository.DataState
+import com.poetcodes.googlekeepclone.ui.view_models.MainViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -18,24 +17,41 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class NotesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentNotesBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+    private val mainViewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notes, container, false)
+        _binding = FragmentNotesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpObservers()
+    }
+
+    private fun setUpObservers() {
+        mainViewModel.notesDataState.observe(requireActivity(), { notesDataState ->
+            when (notesDataState) {
+                is DataState.Error -> TODO()
+                is DataState.Loading -> TODO()
+                is DataState.Success -> TODO()
+            }
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
@@ -43,18 +59,10 @@ class NotesFragment : Fragment() {
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
          *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
          * @return A new instance of fragment NotesFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NotesFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() = NotesFragment()
     }
 }
